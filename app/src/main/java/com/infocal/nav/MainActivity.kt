2c14de92cc5541a2b6954a12a94cd2a1dd1e3c51
jhonathan55 <jfgutierrezsaico@gmail.com>
2026-02-26 22:09:55 -0300

    first commit

---
+++ app/src/main/java/com/infocal/nav/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.infocal.nav
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.infocal.nav.databinding.MainActivityBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MainActivityBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
+
