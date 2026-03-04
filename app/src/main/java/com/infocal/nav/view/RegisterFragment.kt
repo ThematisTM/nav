61e120fb2ee0fe11a0ae40f9496c20b464f7fd9b
jhonathan55 <jfgutierrezsaico@gmail.com>
2026-03-03 21:54:58 -0300

    dev-navController

--- app/src/main/java/com/infocal/nav/view/RegisterFragment.kt
+++ app/src/main/java/com/infocal/nav/view/RegisterFragment.kt
@@ -5,13 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.infocal.nav.R
-import com.infocal.nav.databinding.FragmentLoginBinding
+import com.infocal.nav.databinding.FragmentRegisterBinding
 
 
-class LoginFragment : Fragment() {
-    private var _binding: FragmentLoginBinding?=null
+class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding?=null
     private val binding get()=_binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,12 +22,9 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-       _binding= FragmentLoginBinding.inflate(inflater,container,false)
-        binding.registerButton.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
-        }
+        _binding= FragmentRegisterBinding.inflate(inflater,container,false)
         return binding.root
+
     }
 
 
